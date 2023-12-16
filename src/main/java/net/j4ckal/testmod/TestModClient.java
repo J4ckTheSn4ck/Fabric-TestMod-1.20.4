1f7b95e4fb8db4583a6552b86c7c4a1bea1eb4f3
J4ckTheSn4ck <jacksonbenoitlong@gmail.com>
2023-12-16 17:35:14 -0600

    Initial commit

---
+++ src/main/java/net/j4ckal/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.j4ckal.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
